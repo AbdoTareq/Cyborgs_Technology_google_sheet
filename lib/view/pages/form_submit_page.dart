e4b68e2f5e07da8ce5d62e6f9ee774d78054995e
Abdelrahman Tareq <abdotarekfathy@gmail.com>
2021-01-12 21:32:29 +0200

    feat: Add form validation

--- lib/view/pages/form_submit_page.dart
+++ lib/view/pages/form_submit_page.dart
@@ -15,46 +15,53 @@ class FormSubmitPage extends GetView<GSheetController> {
       ),
       body: Container(
           padding: const EdgeInsets.all(8.0),
-          child: ListView(
-            shrinkWrap: true,
-            children: [
-              TextInput(
-                tEController: controller.nameTextController,
-                hint: 'name',
-              ),
-              TextInput(
-                tEController: controller.mailTextController,
-                hint: 'mail',
-              ),
-              TextInput(
-                tEController: controller.mobileTextController,
-                hint: 'mobile',
-                inputType: TextInputType.number,
-              ),
-              TextInput(
-                tEController: controller.modelNumTextController,
-                hint: 'model number',
-                inputType: TextInputType.number,
-              ),
-              CalendarDatePicker(
-                  initialDate: DateTime.now(),
-                  firstDate: DateTime.now().subtract(Duration(days: 5000)),
-                  lastDate: DateTime.now(),
-                  onDateChanged: (DateTime newDate) {
-                    controller.purchaseDate = newDate.toString();
-                    logger.i("${controller.purchaseDate}");
-                  }),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  RaisedButton(
-                      child: 'submit'.tr.text.make(), onPressed: controller.submitSheetInfo),
-                  RaisedButton(
-                      child: 'Form List screen'.tr.text.make(),
-                      onPressed: () => Get.to(FormListPage())),
-                ],
-              ),
-            ],
+          child: Form(
+            key: controller.formKey,
+            child: ListView(
+              shrinkWrap: true,
+              children: [
+                TextInput(
+                  tEController: controller.nameTextController,
+                  hint: 'name',
+                  validateMessage: 'empty',
+                ),
+                TextInput(
+                  tEController: controller.mailTextController,
+                  hint: 'mail',
+                  validateMessage: 'empty',
+                ),
+                TextInput(
+                  tEController: controller.mobileTextController,
+                  hint: 'mobile',
+                  validateMessage: 'empty',
+                  inputType: TextInputType.number,
+                ),
+                TextInput(
+                  tEController: controller.modelNumTextController,
+                  hint: 'model number',
+                  validateMessage: 'empty',
+                  inputType: TextInputType.number,
+                ),
+                CalendarDatePicker(
+                    initialDate: DateTime.now(),
+                    firstDate: DateTime.now().subtract(Duration(days: 5000)),
+                    lastDate: DateTime.now(),
+                    onDateChanged: (DateTime newDate) {
+                      controller.purchaseDate = newDate.toString();
+                      logger.i("${controller.purchaseDate}");
+                    }),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    RaisedButton(
+                        child: 'submit'.tr.text.make(), onPressed: controller.submitSheetInfo),
+                    RaisedButton(
+                        child: 'Form List screen'.tr.text.make(),
+                        onPressed: () => Get.to(FormListPage())),
+                  ],
+                ),
+              ],
+            ),
           )),
     );
   }
